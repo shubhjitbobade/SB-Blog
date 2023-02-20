2bb4ecb06ebf716e78981191b3bc86813feb69f0
shubhjitbobade <shubhajitbobade8859@gmail.com>
2023-02-20 11:02:52 +0530

    route Parameters ( linking pages)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Navbar from './Navbar';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BlogDetails from './BlogDetails';
 
 
 function App(props) {
@@ -19,6 +20,9 @@ function App(props) {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
