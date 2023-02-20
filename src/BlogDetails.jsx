b391955c26bfd803dfd4492c6f10519194315731
shubhjitbobade <shubhajitbobade8859@gmail.com>
2023-02-20 17:18:50 +0530

    Resuing custom hook

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,13 +1,22 @@
 import { useParams } from "react-router-dom";
-
+import useFeatch from "./UseFetch";
 
 const BlogDetails = () => {
-    const {id}=useParams();
-    return ( 
-        <div className="bolg-details">
-            <h2>Blog Details - {id}</h2>
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFeatch('http://localhost:8000/blogs/' + id);
+    return (
+        <div className="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.auther}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default BlogDetails;
