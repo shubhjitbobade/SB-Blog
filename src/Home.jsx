7b17610c09bed90092120e03354331f408ced3c8
shubhjitbobade <shubhajitbobade8859@gmail.com>
2023-02-20 09:13:38 +0530

    Router Exact match Routes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 import useFeatch from './UseFetch';
 
 function Home() {
     const { data: blogs, isPending, error } = useFeatch(' http://localhost:8000/blogs');
+
     return (
         <div className="home">
             {error && <div>{error}</div>}
